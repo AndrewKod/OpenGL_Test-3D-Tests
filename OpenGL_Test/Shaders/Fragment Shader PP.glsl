b9caec413e0d17843772b2f7ad93b08c07e05268
AndrewKod <AndrewKod@i.ua>
2020-08-25 12:04:10 +0300

    Update Fragment Shader PP.glsl

--- OpenGL_Test/Shaders/Fragment Shader PP.glsl
+++ OpenGL_Test/Shaders/Fragment Shader PP.glsl
@@ -7,8 +7,24 @@ uniform sampler2D screenTexture;
 
 uniform bool bPostProc;
 uniform float kernel[9];
+uniform vec2 uvOffset;
 
 void main()
 { 
-    FragColor = texture(screenTexture, TexCoords);
+	vec4 col = vec4(0.0);
+	if(!bPostProc)
+		col = texture(screenTexture, TexCoords);
+	else
+	{	
+		int kernelID = 0;
+		for(int i = -1; i <= 1; i++)
+		{
+			for(int j = -1; j <= 1; j++)
+			{
+				col += vec4(vec3(texture(screenTexture, TexCoords.st + vec2(uvOffset.x * j, uvOffset.y * i))) * kernel[kernelID], 1.0);
+				kernelID++;				
+			}
+		}			
+	}
+	FragColor = col;
 }
